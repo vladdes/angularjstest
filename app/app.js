6ccae8a7df58d3a6a0e246e6ba66c80a195abc7e
vladimir strukelj <vladimir.strukelj@gmail.com>
2018-10-24 21:24:46 +0200

    asd

--- app/app.js
+++ app/app.js
@@ -1,5 +1,18 @@
 (() => {
     
-    var app = angular.module('customersApp', []);
-    
+    var app = angular.module('customersApp', ['ngRoute']);
+    app.config(($routeProvider) => {
+        $routeProvider
+            .when('/', 
+            {
+                controller: 'CustomersController',
+                templateUrl: '/app/views/customers.html'
+            })
+            .when('/order',
+                {
+                controller: 'OrdersController',
+                templateUrl: '/app/views/orders.html'
+            })
+            .otherwise({ redirectTo: '/'});
+    });
 })();
